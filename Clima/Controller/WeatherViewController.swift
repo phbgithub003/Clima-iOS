03cdfce88cfbb605ec57dfff0a58bb5198445609
harshitp <palacheralaharshit@gmail.com>
2024-02-09 16:34:30 +0530

    initial commit

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Harshit on 09/02/2024.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    editi
+
+}
+
